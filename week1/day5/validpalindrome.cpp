2ea18fb6a761a983c2afa1b316e8f3f181159c1b
bhawna0419-sketch <bhawna0419@gmail.com>
2026-06-23 23:49:39 +0530

    Create validpalindrome.cpp

---
+++ week1/day5/validpalindrome.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool valid(char ch) {
+        if ((ch >= 'a' && ch <= 'z') || 
+            (ch >= 'A' && ch <= 'Z') || 
+            (ch >= '0' && ch <= '9')) {
+            return true;
+        }
+        return false;
+    }
+
+    char toLowerCase(char ch) {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
+            return ch;
+        else
+            return ch - 'A' + 'a';
+    }
+
+    bool isPalindrome(string s) {
+        string temp = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (valid(s[i])) {
+                temp.push_back(s[i]);
+            }
+        }
+
+        for (int i = 0; i < temp.length(); i++) {
+            temp[i] = toLowerCase(temp[i]);
+        }
+
+        int start = 0;
+        int end = temp.length() - 1;
+
+        while (start < end) {
+            if (temp[start] != temp[end])
+                return false;
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+};
